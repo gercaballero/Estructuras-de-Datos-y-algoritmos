3bf370830df25f8852a6d13d9eb52ce30ae208b5
gercaballero <83521789+gercaballero@users.noreply.github.com>
2021-08-28 14:33:14 -0300

    Delete Ej2.py

--- UNIDAD 3/Practica 2- Pila y Cola/Ejercicio 2/Ej2.py
+++
@@ -1,7 +0,0 @@
-from classPilaEncadenada import PilaEncadenada
-from Conversor import conversor
-import os
-if __name__=='__main__':
-    os.system('cls')
-    num=int(input('Ingrese un numero decimal: '))
-    conversor(num)
