ec5ed3ad8428db5ec5be01daab229787296953b7
gercaballero <83521789+gercaballero@users.noreply.github.com>
2021-08-30 10:37:14 -0300

    Add files via upload

--- UNIDAD 3/Practica 2- Pila y Cola/Ejercicio 5/classPila.py
+++ UNIDAD 3/Practica 2- Pila y Cola/Ejercicio 5/classPila.py
@@ -1,44 +1,97 @@
-class Pila:
-    __items=[]
-    __tope=None
+class PilaSecuencial:
+    __items = None
+    __tope = -1
+    __cant = 0
+
+    def __init__(self,xcant=0):
+        self.__items = []
+        self.__cant = xcant
+        self.__tope = -1
+    
+    def vacia(self):
+        vacia = False
+        if(self.__tope == -1):
+            vacia = True
+        return vacia
+
+    def llena(self):
+        llena = False
+        if(self.__tope == self.__cant-1):
+            llena = True
+        return llena 
+
+    def insertar(self,x):
+        if(not self.llena()):
+            self.__items.append(x)
+            self.__tope = self.__tope + 1
+        else:
+            print("Pila llena")
+    
+    def suprimir(self):
+        if(self.vacia()):
+            return None
+        else:
+            x = self.__items.pop()
+            self.__tope = self.__tope - 1
+            return x
+
+class Nodo:
+    __item=None
+    __siguiente=None
+    def __init__(self) -> None:
+        self.__item=None
+        self.__siguiente=None
+    
+    def obtenerItem(self):
+        return(self.__item)
+    
+    def cargarItem(self,xitem):
+        self.__item=xitem
+
+    def cargarSig(self,sig):
+        if isinstance(sig,Nodo):
+            self.__siguiente=sig
+    
+    def obtenerSig(self):
+        return self.__siguiente
+
+class PilaEncadenada:
     __cant=None
+    __tope=None
 
-    def __init__(self,cant=0) -> None:
-        self.__items=[]
-        self.__cant=cant
-        self.__tope=-1
-
+    def __init__(self,xtope=None,xcant=0) -> None:
+        self.__cant=xcant
+        self.__tope=xtope
+    
     def vacia(self):
         vacia=False
-        if self.__tope==-1:
+        if self.__tope==None:
             vacia=True
         return vacia
-    def insertar(self,item):
-        if self.__tope< (self.__cant-1):
-            self.__items[self.__tope+1]
-            self.__tope=self.__tope+1
-            return item
+    
+    def insertar(self,x):
+        nodo=Nodo()
+        nodo.cargarItem(x)
+        nodo.cargarSig(self.__tope)
+        self.__tope=nodo
+        self.__cant=self.__cant+1
+        return nodo.obtenerItem()
+
+    def suprimir(self):
+        x=None
+        if self.vacia():
+            print('Pila Vacia')
         else:
-            return 0
-'''
-int suprimir(void)
-{ int x;
-if (vacia())
-{ printf("%s","Pila vacia");
-return(0);
-}
-else
-{ x=items[tope--];
-return(x);
-}
-}
-void mostrar(void)
-{ int i;
-if (!vacia())
-{ for (i=tope; i>=0; i--)
-{ cout<<items[i]<<endl;
-}
-}
-}
-};
-'''
+            x=self.__tope.obtenerItem()
+            self.__tope=self.__tope.obtenerSig()
+            self.__cant=self.__cant-1
+            return x
+
+    def mostrar(self):
+        while not self.vacia():
+            print(self.suprimir())
+    
+    def listar(self):
+        while not self.vacia():
+            print(self.__tope)
+
