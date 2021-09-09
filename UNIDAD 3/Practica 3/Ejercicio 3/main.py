59978d7eb5fc5708cb47e970a367927fb66eae5c
gercaballero <83521789+gercaballero@users.noreply.github.com>
2021-09-08 23:52:49 -0300

    Add files via upload

---
+++ UNIDAD 3/Practica 3/Ejercicio 3/main.py
@@ -0,0 +1,25 @@
+import imp
+from ListaContenido import Lista
+from classDato import Dato
+import csv
+import os
+if __name__=='__main__':
+    os.system('cls')
+    archivo=open('archivo.csv')
+    numline = len(archivo.readlines()) #Obtenemos la cantidad de lineas
+    archivo=open('archivo.csv')        #Como se sierra el archivo lo volvemos a abrir
+    reader=csv.reader(archivo,delimiter=';')
+    bandera=True
+    lista=Lista(numline,Dato)
+    for fila in reader:
+        if bandera:
+            bandera=False
+        else:
+            prov=fila[3]
+            sup=fila[6]
+            nuevo=Dato(prov,sup)
+            lista.insertar(nuevo)
+    archivo.close()
+    print('{0:^20}{1:^10}'.format('Provincia','Superficie (ha)'))
+    print('---------------------------------------------')
+    lista.recorrer()
